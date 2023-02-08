d6cb0dcf4fc10bba456879a5a31e5476b54de688
Joliwood <jolibois.contact@gmail.com>
2023-02-08 17:19:48 +0100

    :hammer: home + headerbar implemanted but not responsive

--- src/components/headerBar/index.js
+++ src/components/headerBar/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./styles.css";
 
 function HeaderBar() {
-  return <div>HeaderBar</div>;
+  return (
+    <div className="headerBarArea">
+      <h2>Mon portfolio</h2>
+
+      <div className="headerBarButtonsEnsemble">
+        <button className="headerBarButton">
+          <h3>Accueil</h3>
+        </button>
+
+        <button className="headerBarButton">
+          <h3>Compétences</h3>
+        </button>
+
+        <button className="headerBarButton">
+          <h3>Portfolio</h3>
+        </button>
+
+        <button className="headerBarButton">
+          <h3>Contact</h3>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderBar;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import GithubLogo from "../svg/githubLogo";
+import LinkedinLogo from "../svg/linkedinLogo";
+import ProfilePicture from "../images/profilePicture.jpeg";
+import "./styles.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="homeArea">
+      <div className="homeDescription">
+        <h1>Guillaume Jolibois</h1>
+        <h2>Développeur Front-End</h2>
+        <p>
+          Autodidacte de 27 ans, avec une appetence pour le design et
+          l’ergonomie.
+        </p>
+        <div className="homeLogosLinks">
+          <LinkedinLogo />
+          <GithubLogo />
+        </div>
+      </div>
+
+      <img
+        src={ProfilePicture}
+        alt="profile photo"
+        className="homeProfilePicture unselectable"
+      />
+    </div>
+  );
 }
 
 export default Home;

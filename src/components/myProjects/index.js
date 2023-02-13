d5b33316cc71af8848e451d306617e55f05fcd16
Joliwood <jolibois.contact@gmail.com>
2023-02-13 17:38:35 +0100

    :hammer: my projects section added

--- src/components/myProjects/index.js
+++ src/components/myProjects/index.js
@@ -1,7 +1,61 @@
 import React from "react";
+import "./styles.css";
+import NetflixProject from "../images/projects/netflixProject.png";
 
 function MyProjects() {
-  return <div>MyProjects</div>;
+  return (
+    <div className="myProjectsArea">
+      <div className="competencesSeparationEnsemble">
+        <div className="competencesSeparationbar"></div>
+        <h2>My Projects</h2>
+        <div className="competencesSeparationbar"></div>
+      </div>
+
+      <div className="myProjectsContainer">
+        <div className="myProjectIndividualContainer">
+          <a
+            href="https://storied-biscochitos-463f24.netlify.app/"
+            className="myProjectsProjectImgLink"
+          >
+            <img
+              src={NetflixProject}
+              alt="Netflix project"
+              className="myProjectsPicture"
+            />
+          </a>
+          <a
+            href="https://storied-biscochitos-463f24.netlify.app/"
+            className="myProjectsProjectTitleLink"
+          >
+            <h3 className="myProjectsTitle">
+              Netflix Clone - React.js + API + Netlify
+            </h3>
+          </a>
+        </div>
+
+        <div className="myProjectIndividualContainer">
+          <a
+            href="https://storied-biscochitos-463f24.netlify.app/"
+            className="myProjectsProjectImgLink"
+          >
+            <img
+              src={NetflixProject}
+              alt="Netflix project"
+              className="myProjectsPicture"
+            />
+          </a>
+          <a
+            href="https://storied-biscochitos-463f24.netlify.app/"
+            className="myProjectsProjectTitleLink"
+          >
+            <h3 className="myProjectsTitle">
+              Spotify Clone - React.js + API + Netlify
+            </h3>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyProjects;

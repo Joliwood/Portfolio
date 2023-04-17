6f41d08ccdabad4cd348eb28ab4b260de5478f51
Guillaume <jolibois.contact@gmail.com>
2023-04-17 17:08:03 +0200

    :hammer: home section border animate CSS

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -7,26 +7,30 @@ import "./styles.css";
 function Home() {
   return (
     <div className="homeArea" id="homeArea">
-      <div className="homeDescription">
-        <h1>Guillaume Jolibois</h1>
-        <h2>Développeur Javascript</h2>
-        <p>
-          Autodidacte de 28 ans, avec une appetence pour le design et
-          l’ergonomie. Je souhaite mettre mes compétences aux services des
-          entreprises. Disponible en région parisienne.
-        </p>
-        <div className="homeLogosLinks">
-          <a
-            href="https://www.linkedin.com/in/guillaume-jolibois-430426119/"
-            target="blank"
-          >
-            <LinkedinLogo />
-          </a>
-          <a href="https://github.com/Joliwood" target="blank">
-            <GithubLogo />
-          </a>
+      <div className="homeDescriptionContainer">
+        <div className="homeDescription">
+          <h1>Guillaume Jolibois</h1>
+          <h2>Développeur Javascript</h2>
+          <p>
+            Autodidacte de 28 ans, avec une appetence pour le design et
+            l’ergonomie. Je souhaite mettre mes compétences aux services des
+            entreprises. Disponible en région parisienne.
+          </p>
+          <div className="homeLogosLinks">
+            <a
+              href="https://www.linkedin.com/in/guillaume-jolibois-430426119/"
+              target="blank"
+            >
+              <LinkedinLogo />
+            </a>
+            <a href="https://github.com/Joliwood" target="blank">
+              <GithubLogo />
+            </a>
+          </div>
         </div>
       </div>
+
+      <div className="homeLine"></div>
 
       <img
         src={ProfilePicture}

bbfc3d7203ea85e5210252cb72a009df76e11d44
Guillaume <jolibois.contact@gmail.com>
2023-04-25 02:34:55 +0200

    :hammer: button return top page implemented

---
+++ components/returnTopScroller/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/headerBar.module.css";
+import TopAngle from "../svg/topAngle";
+import { Link as ScrollLink } from "react-scroll";
+
+function ReturnTopScroller() {
+  const [pageY, setPageY] = useState(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setPageY(window.pageYOffset);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <ScrollLink
+      to="homeArea"
+      spy={true}
+      smooth={true}
+      offset={-250}
+      duration={500}
+      href="#home-area"
+    >
+      <div
+        className={`${styles.returnTopScrollContainer} ${
+          pageY > 200 ? styles.animatedAppears : ""
+        } ${pageY <= 200 ? styles.animatedDisappears : ""}`}
+      >
+        <TopAngle />
+      </div>
+    </ScrollLink>
+  );
+}
+
+export default ReturnTopScroller;

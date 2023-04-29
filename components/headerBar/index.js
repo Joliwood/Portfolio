a7c9aea063454af763bc0784fc7c72f809ab897d
Guillaume <jolibois.contact@gmail.com>
2023-04-29 17:57:26 +0200

    :construction: WIP light & dark mode

--- components/headerBar/index.js
+++ components/headerBar/index.js
@@ -3,6 +3,8 @@ import styles from "../../styles/headerBar.module.css";
 import stylesAnimations from "../../styles/animations.module.css";
 import { IoClose } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { MdOutlineNightsStay } from "react-icons/md";
+import { BsSun } from "react-icons/bs";
 import { Link as ScrollLink } from "react-scroll";
 import Link from "next/link";
 import PortfolioLogo from "../../public/images/portfolio-logo.png";
@@ -11,6 +13,13 @@ import Image from "next/image";
 const HeaderBar = function () {
   const [toggleMenu, setToggleMenu] = useState(true);
   const [animatedMenu, setAnimatedMenu] = useState(null);
+  const [lightTheme, setLightTheme] = useState(false);
+
+  const changeTheme = () => {
+    const rootTheme = document.querySelector(":root");
+    rootTheme.classList.toggle("lightTheme");
+    setLightTheme(!lightTheme);
+  };
 
   return (
     <div className={styles.headerBarArea}>
@@ -73,7 +82,6 @@ const HeaderBar = function () {
             <h3>Accueil</h3>
           </button>
         </ScrollLink>
-
         <ScrollLink
           to="competencesArea"
           spy={true}
@@ -86,7 +94,6 @@ const HeaderBar = function () {
             <h3>Compétences</h3>
           </button>
         </ScrollLink>
-
         <ScrollLink
           to="myProjectsArea"
           spy={true}
@@ -99,12 +106,19 @@ const HeaderBar = function () {
             <h3>Portfolio</h3>
           </button>
         </ScrollLink>
-
         <button className={styles.headerBarButton}>
           <Link href="/contact">
             <h3>Contact</h3>
           </Link>
         </button>
+        {lightTheme ? (
+          <MdOutlineNightsStay
+            className={styles.themeIcon}
+            onClick={changeTheme}
+          />
+        ) : (
+          <BsSun className={styles.themeIcon} onClick={changeTheme} />
+        )}
       </div>
     </div>
   );

3e064075eaf4dc14f67c69ce004840dd5305ed59
Guillaume <jolibois.contact@gmail.com>
2023-05-24 21:58:42 +0200

    :hammer: logo changed

--- components/headerBar/index.js
+++ components/headerBar/index.js
@@ -7,8 +7,9 @@ import { MdOutlineNightsStay } from "react-icons/md";
 import { BsSun } from "react-icons/bs";
 import { Link as ScrollLink } from "react-scroll";
 import Link from "next/link";
-import PortfolioLogo from "../../public/images/portfolio-logo.png";
-import Image from "next/image";
+// import PortfolioLogo from "../../public/images/portfolio-logo.png";
+// import Image from "next/image";
+import Logo from "../svg/logo/index.js";
 
 const HeaderBar = function () {
   const [toggleMenu, setToggleMenu] = useState(true);
@@ -33,7 +34,9 @@ const HeaderBar = function () {
           duration={500}
           href="#home-area"
         >
-          <Image src={PortfolioLogo} alt="GJ" title="Logo" />
+          {/* <Image src={PortfolioLogo} alt="GJ" title="Logo" /> */}
+          {/* <Image src={LogoWebsite} alt="GJ" title="Logo" /> */}
+          <Logo />
         </ScrollLink>
 
         <div className={styles.themeSwitchContainerSmallDevices}>
---
+++ components/svg/logo/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import styles from "../../../styles/svg.module.scss";
+
+function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      xmlSpace="preserve"
+      className={styles.logo}
+      viewBox="0 0 33.374 32.808"
+    >
+      <defs>
+        <clipPath id="clipPath25292" clipPathUnits="userSpaceOnUse">
+          <path d="M347.9 426.183c23.507-2.745 50.487-11.173 48.04-38.987-1.704-19.369-15.987-31.306-31.559-28.006-18.303 3.878-23.992 24.65-11.461 37.067 13.421 13.295 30.643.548 30.643.548-9.327 12.393-30.245 12.158-40.526-.548-9.996-12.356-10.519-38.181 13.511-51.345 26.733-14.644 64.534 12.579 55.124 49.424-7.331 28.704-28.617 34.493-44.595 34.492-10.801 0-19.177-2.645-19.177-2.645"></path>
+        </clipPath>
+        <linearGradient
+          id="linearGradient25302"
+          x1="0"
+          x2="1"
+          y1="0"
+          y2="0"
+          gradientTransform="matrix(76.58887 0 0 -76.58887 336.554 384.854)"
+          gradientUnits="userSpaceOnUse"
+          spreadMethod="pad"
+        >
+          <stop offset="0" stopColor="#cfaa7a" stopOpacity="1"></stop>
+          <stop offset="1" stopColor="#ad8b61" stopOpacity="1"></stop>
+        </linearGradient>
+        <clipPath id="clipPath25312" clipPathUnits="userSpaceOnUse">
+          <path d="M318.992 373.193c3.512-31.026 36.203-37.365 36.203-37.365-30.371 9.729-38.472 42.133-23.285 63.723 13.899 19.763 35.985 17.298 35.985 17.298-8.647 4.118-24.046 6.316-24.046 6.316-10.672-4.641-28.368-18.946-24.857-49.972"></path>
+        </clipPath>
+        <linearGradient
+          id="linearGradient25322"
+          x1="0"
+          x2="1"
+          y1="0"
+          y2="0"
+          gradientTransform="matrix(49.35547 0 0 -49.35547 318.54 379.496)"
+          gradientUnits="userSpaceOnUse"
+          spreadMethod="pad"
+        >
+          <stop offset="0" stopColor="#414042" stopOpacity="1"></stop>
+          <stop offset="1" stopColor="#a6a8ab" stopOpacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <g transform="matrix(.35278 0 0 -.35278 -112.373 151.281)">
+        <g clipPath="url(#clipPath25292)">
+          <g>
+            <g>
+              <path
+                fill="url(#linearGradient25302)"
+                stroke="none"
+                d="M347.9 426.183c23.507-2.745 50.487-11.173 48.04-38.987-1.704-19.369-15.987-31.306-31.559-28.006-18.303 3.878-23.992 24.65-11.461 37.067 13.421 13.295 30.643.548 30.643.548-9.327 12.393-30.245 12.158-40.526-.548-9.996-12.356-10.519-38.181 13.511-51.345 26.733-14.644 64.534 12.579 55.124 49.424-7.331 28.704-28.617 34.493-44.595 34.492-10.801 0-19.177-2.645-19.177-2.645"
+              ></path>
+            </g>
+          </g>
+        </g>
+      </g>
+      <g transform="matrix(.35278 0 0 -.35278 -112.373 151.281)">
+        <g clipPath="url(#clipPath25312)">
+          <g>
+            <g>
+              <path
+                fill="url(#linearGradient25322)"
+                stroke="none"
+                d="M318.992 373.193c3.512-31.026 36.203-37.365 36.203-37.365-30.371 9.729-38.472 42.133-23.285 63.723 13.899 19.763 35.985 17.298 35.985 17.298-8.647 4.118-24.046 6.316-24.046 6.316-10.672-4.641-28.368-18.946-24.857-49.972"
+              ></path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default Logo;

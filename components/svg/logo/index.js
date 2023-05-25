01d0150e8a1f982fd64f96fa7938eeece3a6041f
Guillaume <jolibois.contact@gmail.com>
2023-05-25 15:25:30 +0200

    :zap: better logo

--- components/svg/logo/index.js
+++ components/svg/logo/index.js
@@ -9,48 +9,48 @@ function Logo() {
       version="1.1"
       xmlSpace="preserve"
       className={styles.logo}
-      viewBox="0 0 178.479 175.454"
+      viewBox="0 0 674.568 663.131"
     >
       <defs>
-        <linearGradient
-          id="linearGradient25322"
-          x1="0"
-          x2="1"
-          y1="0"
-          y2="0"
-          gradientTransform="matrix(93.11385 0 0 93.11385 14.185 145.15)"
-          gradientUnits="userSpaceOnUse"
-          spreadMethod="pad"
-        >
-          <stop offset="0" stopColor="#414042" stopOpacity="1"></stop>
-          <stop offset="1" stopColor="#a6a8ab" stopOpacity="1"></stop>
-        </linearGradient>
         <linearGradient
           id="linearGradient25302"
           x1="0"
           x2="1"
           y1="0"
           y2="0"
-          gradientTransform="translate(48.171 135.042) scale(144.49228)"
+          gradientTransform="translate(279.34 358.847) scale(546.11256)"
           gradientUnits="userSpaceOnUse"
           spreadMethod="pad"
         >
           <stop offset="0" stopColor="#cfaa7a" stopOpacity="1"></stop>
           <stop offset="1" stopColor="#ad8b61" stopOpacity="1"></stop>
         </linearGradient>
+        <linearGradient
+          id="linearGradient25322"
+          x1="0"
+          x2="1"
+          y1="0"
+          y2="0"
+          gradientTransform="translate(150.888 397.048) scale(351.92635)"
+          gradientUnits="userSpaceOnUse"
+          spreadMethod="pad"
+        >
+          <stop offset="0" stopColor="#414042" stopOpacity="1"></stop>
+          <stop offset="1" stopColor="#a6a8ab" stopOpacity="1"></stop>
+        </linearGradient>
       </defs>
       <g
         stroke="none"
-        strokeWidth="1.887"
-        transform="translate(-14.184 -52.08)"
+        strokeWidth="7.13"
+        transform="translate(-150.885 -45.29)"
       >
         <path
           fill="url(#linearGradient25302)"
-          d="M69.577 57.07c44.348 5.178 95.249 21.079 90.632 73.553-3.215 36.541-30.16 59.061-59.539 52.836-34.53-7.317-45.263-46.505-21.622-69.931 25.32-25.082 57.81-1.034 57.81-1.034-17.596-23.38-57.06-22.937-76.455 1.034-18.859 23.31-19.846 72.032 25.49 96.867 50.434 27.628 121.749-23.731 103.996-93.243C176.06 63 135.9 52.078 105.756 52.08c-20.377 0-36.179 4.99-36.179 4.99"
+          d="M360.245 64.15C527.86 83.723 720.24 143.818 702.791 342.145c-12.15 138.109-113.994 223.225-225.03 199.695-130.508-27.652-171.073-175.766-81.721-264.304 95.697-94.8 218.498-3.908 218.498-3.908-66.506-88.368-215.66-86.692-288.968 3.908-71.276 88.103-75.006 272.247 96.339 366.112 190.618 104.418 460.156-89.694 393.059-352.415C762.694 86.561 610.916 45.283 496.986 45.29c-77.017 0-136.741 18.86-136.741 18.86"
         ></path>
         <path
           fill="url(#linearGradient25322)"
-          d="M15.04 157.04c6.625 58.534 68.3 70.493 68.3 70.493-57.298-18.355-72.581-79.488-43.93-120.22C65.632 70.03 107.3 74.68 107.3 74.68 90.986 66.91 61.934 62.763 61.934 62.763 41.801 71.52 8.416 98.507 15.04 157.041"
+          d="M154.118 441.992c25.042 221.23 258.144 266.43 258.144 266.43-216.56-69.373-274.323-300.428-166.033-454.374 99.106-140.92 256.59-123.343 256.59-123.343-61.658-29.363-171.46-45.036-171.46-45.036-76.096 33.093-202.276 135.094-177.24 356.323"
         ></path>
       </g>
     </svg>

7c912ed867a2340891a32406fd8cfe28d8064191
Guillaume <jolibois.contact@gmail.com>
2023-05-25 18:49:37 +0200

    :hammer: competences + projects updated + minor modification in light mode colors

--- components/myProjects/index.js
+++ components/myProjects/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "../../styles/myProjects.module.scss";
+import stylesExperiences from "../../styles/myExperiences.module.scss";
 import { MyProjectsList } from "./myProjectsList";
 import Image from "next/image";
+import GithubLogo from "../svg/githubLogo";
 
 function MyProjects() {
   return (
@@ -13,18 +15,43 @@ function MyProjects() {
       </div>
 
       <div className={styles.myProjectsContainer}>
-        {MyProjectsList.map((data, index) => (
-          <a href={data.link} target="blank" key={index} data-aos="fade-up">
+        {MyProjectsList.reverse().map((data, index) => (
+          <div key={index} data-aos="fade-up">
             <div className={styles.myProjectIndividualContainer}>
-              <Image
-                src={data.img}
-                alt={data.imgTag}
-                title={data.imgTag}
-                className={styles.myProjectsPicture}
-              />
-              <h3 className={styles.myProjectsTitle}>{data.title}</h3>
+              <div>
+                <Image
+                  src={data.img}
+                  alt={data.imgTag}
+                  title={data.imgTag}
+                  className={styles.myProjectsPicture}
+                />
+
+                <div className={styles.projectGithubButtons}>
+                  <a href={data.github} target="blank">
+                    <h4>Voir le Github</h4>
+                  </a>
+                  <a href={data.link} target="blank">
+                    <h4>Voir le Projet</h4>
+                  </a>
+                </div>
+              </div>
+              <div>
+                <h3 className={styles.myProjectsTitle}>{data.title}</h3>
+                <div className={styles.myProjectStacksContainer}>
+                  {data.stacks.map((stack, stackIndex) => (
+                    <Image
+                      src={"/images/competence/" + stack + "Logo.png"}
+                      alt={stack}
+                      title={stack}
+                      width={35}
+                      height={35}
+                      key={stackIndex}
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
-          </a>
+          </div>
         ))}
       </div>
     </div>

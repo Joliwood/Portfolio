61b6faecb62dded101556e5e3b28ef96647c6c68
Guillaume <jolibois.contact@gmail.com>
2023-06-26 16:03:02 +0200

    :zap: image debuged next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
     domains: ["github.com"],
   },
 };
